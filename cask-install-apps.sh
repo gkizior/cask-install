a30d1d6677258f4793ef56c0b51542cc5add7a80
Garrett Kizior <kiziorgarrett@gmail.com>
2019-05-20 07:51:00 -0500

    cask install apps script

---
+++ cask-install-apps.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+brew cask install discord
+brew cask install docker
+brew cask install google-chrome
+brew cask install microsoft-teams
+brew cask install postman
+brew cask install slack
+brew cask install sourcetree
+brew cask install spotify
+brew cask install visual-studio-code
+brew cask install vlc
